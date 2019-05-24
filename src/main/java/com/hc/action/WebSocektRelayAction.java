479104e977d13d094e0745fe2f56bf392e39e0fb
tony <tony@gmail.com>
2019-05-24 20:38:19 +0800

    完善了查询数据的BUG

---
+++ src/main/java/com/hc/action/WebSocektRelayAction.java
@@ -0,0 +1,59 @@
+package com.hc.action;
+
+import com.hc.bean.DoMain;
+import com.hc.service.ISocketService;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import net.sf.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by GOPENEDD on 2019/5/19
+ */
+public class WebSocektRelayAction extends ActionSupport implements ModelDriven<DoMain> {
+
+    private DoMain dm = new DoMain();
+    private ISocketService is ;
+
+    HttpServletRequest request = (HttpServletRequest) ActionContext.getContext()
+            .get(org.apache.struts2.StrutsStatics.HTTP_REQUEST);
+    HttpServletResponse response = (HttpServletResponse) ActionContext.getContext()
+            .get(org.apache.struts2.StrutsStatics.HTTP_RESPONSE);
+
+    /**
+     * cd.md.dt_id  数据ID        Integer
+     * u.vd_secret  播放密码       String
+     *
+     * return:
+     * status       200             Integer     "ok"
+     * status       400             Integer     "err"
+     * @throws Exception
+     */
+    public void open() throws Exception {
+        JSONObject jso = is.doShellAndRestore(dm);
+        response.getWriter().print(jso);
+    }
+
+    /**
+     * cd.md.dt_id  数据ID        Integer
+     *
+     * @throws Exception
+     */
+    public void terminate() throws Exception {
+        JSONObject jso = is.terminateRelay(dm);
+        response.getWriter().print(jso);
+    }
+
+    @Override
+    public DoMain getModel() {
+        return dm;
+    }
+
+    public void setIs(ISocketService is) {
+        this.is = is;
+    }
+}
